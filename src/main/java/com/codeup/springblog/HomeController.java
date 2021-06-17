549163e937277b12c377c3220ba97c6daf127a55
Emmanuel Stephen <emmanuel.r.stephen86@gmail.com>
2021-06-17 11:08:46 -0500

    feat:starting from scratch again

--- src/main/java/com/codeup/springblog/HomeController.java
+++ src/main/java/com/codeup/springblog/HomeController.java
@@ -1,14 +1,5 @@
 package com.codeup.springblog;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
-
-@Controller
 public class HomeController {
 
-    @GetMapping("/")
-    @ResponseBody
-    public String Landing(){
-        return "This is the landing page!";
-    }
 }
